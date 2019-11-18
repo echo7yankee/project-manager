c79d9fe4a71395e675a2c9ea8fa9d3611afe6744
Dragos Ifrim <dragos.ifrim@softvision.ro>
2019-11-18 09:55:39 +0200

    Fixed bug: Task form didn't close when clicking outside

--- frontend/src/Components/Layout/TasksContent/TaskForm.tsx
+++ frontend/src/Components/Layout/TasksContent/TaskForm.tsx
@@ -1,46 +1,53 @@
-import React, { useEffect, SetStateAction } from 'react';
+import React, { SetStateAction, useEffect, useRef } from 'react';
+
+//close component by clicking outside
+import { useOutsideClose } from '../../CloseDropdown/CloseDropdown';
 
 //style
-import style from './tasks.module.css';
 import { DatePickerCalendar } from '../../Datepicker/DatePickerCalendar';
+import style from './tasks.module.css';
 
 interface ITaskForm {
-    buttonDo: string;
-    buttonClose: string;
-    selectedDay: Date;
-    inputValue: string;
-    onClickClose: () => void;
-    handleDayChange;
-    onChange: (e: { target: { value: SetStateAction<string>; }; }) => void;
-    request: (e: { preventDefault: () => void; }) => void;
-    inputRef;
+  buttonDo: string;
+  buttonClose: string;
+  selectedDay: Date;
+  inputValue: string;
+  onClickClose: () => void;
+  handleDayChange;
+  onChange: (e: { target: { value: SetStateAction<string>; }; }) => void;
+  request: (e: { preventDefault: () => void; }) => void;
+  inputRef;
 }
 
 export const TaskForm = (props: ITaskForm): JSX.Element => {
 
-    useEffect(() => {
-        props.inputRef.current.focus();
-    }, [props.inputRef])
+  useEffect(() => {
+    props.inputRef.current.focus();
+  }, [props.inputRef])
 
-    return (
-        <form className={style.taskForm} onSubmit={props.request} >
-            <div className={style.taskFormInputGroup}>
-                <input
-                    type='text'
-                    placeholder="Add a task ... e.g Read tonight's news"
-                    value={props.inputValue}
-                    onChange={props.onChange}
-                    ref={props.inputRef} />
-                <DatePickerCalendar
-                    selectedDay={props.selectedDay}
-                    handleDayChange={props.handleDayChange}
-                />
-            </div>
-            <div className={style.taskFormButtonContainer}>
-                <button disabled={props.inputValue === ''}
-                    className={props.inputValue === '' ? 'disabled-button' : ''}>{props.buttonDo}</button>
-                <button onClick={props.onClickClose}>{props.buttonClose}</button>
-            </div>
-        </form>
-    );
+  //close taskForm
+  const wrapperRef = useRef(null);
+  useOutsideClose(wrapperRef, props.onClickClose);
+
+  return (
+    <form className={style.taskForm} onSubmit={props.request} ref={wrapperRef} >
+      <div className={style.taskFormInputGroup}>
+        <input
+          type='text'
+          placeholder="Add a task ... e.g Read tonight's news"
+          value={props.inputValue}
+          onChange={props.onChange}
+          ref={props.inputRef} />
+        <DatePickerCalendar
+          selectedDay={props.selectedDay}
+          handleDayChange={props.handleDayChange}
+        />
+      </div>
+      <div className={style.taskFormButtonContainer}>
+        <button disabled={props.inputValue === ''}
+          className={props.inputValue === '' ? 'disabled-button' : ''}>{props.buttonDo}</button>
+        <button onClick={props.onClickClose}>{props.buttonClose}</button>
+      </div>
+    </form>
+  );
 };
--- frontend/src/Components/Layout/TasksContent/Tasks.tsx
+++ frontend/src/Components/Layout/TasksContent/Tasks.tsx
@@ -24,137 +24,137 @@ import { Task } from './Task';
 import { TasksHistory } from './TaskHistory/TasksHistory';
 
 export const Tasks = ({ history: { location } }): JSX.Element => {
-    const [toggleTaskForm, setToggleTaskForm] = useState(false);
-    const [taskValue, setTaskValue] = useState('');
-    const [dropdown, setDropdown] = useState(false);
-    const [selectedDay, setSelectedDay] = useState<Date>(new Date());
-    const projectId: string = location.pathname.split('project/').pop();
-    const projectName: string = location.search.split('?').pop();
-    const isArchived: boolean = location.state;
-    const inputRef: any = useRef(null);
+  const [toggleTaskForm, setToggleTaskForm] = useState(false);
+  const [taskValue, setTaskValue] = useState('');
+  const [dropdown, setDropdown] = useState(false);
+  const [selectedDay, setSelectedDay] = useState<Date>(new Date());
+  const projectId: string = location.pathname.split('project/').pop();
+  const projectName: string = location.search.split('?').pop();
+  const isArchived: boolean = location.state;
+  const inputRef: any = useRef(null);
 
-    //redux
-    const tasks = useSelector(state => state.task.tasks);
-    const isLoading = useSelector(state => state.task.isLoading);
-    const showToast = useSelector(state => state.task.showToast);
-    const toastText = useSelector(state => state.task.toastText);
-    const errors = useSelector(state => state.task.errors);
+  //redux
+  const tasks = useSelector(state => state.task.tasks);
+  const isLoading = useSelector(state => state.task.isLoading);
+  const showToast = useSelector(state => state.task.showToast);
+  const toastText = useSelector(state => state.task.toastText);
+  const errors = useSelector(state => state.task.errors);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    //token
-    let decodedToken;
-    let userId;
-    const token = localStorage.FBIdToken;
+  //token
+  let decodedToken;
+  let userId;
+  const token = localStorage.FBIdToken;
 
-    if (token) {
-        decodedToken = jwt.decode(token);
-        userId = decodedToken.params && decodedToken.params.id;
+  if (token) {
+    decodedToken = jwt.decode(token);
+    userId = decodedToken.params && decodedToken.params.id;
+  }
+
+  useEffect(() => {
+    dispatch(getTasks(projectId));
+  }, [dispatch, projectId]);
+
+  function toggleForm(): void {
+    setToggleTaskForm(true)
+  }
+
+  function closeForm(): void {
+    setToggleTaskForm(false);
+  }
+
+  function openDropdown(): void {
+    setDropdown(true);
+  }
+
+  function closeDropdown(): void {
+    setDropdown(false);
+  }
+
+  function handleChange(e: { target: { value: React.SetStateAction<string>; }; }): void {
+    setTaskValue(e.target.value);
+  }
+
+  function createTaskRequest(e: { preventDefault: () => void; }): void {
+    e.preventDefault();
+    const newTask = {
+      task: taskValue,
+      date: new Date(),
+      schedule: selectedDay,
+      projectName,
+      completed: false,
+      archived: isArchived,
+      userId
     }
+    dispatch(createTask(projectId, newTask));
+    setTaskValue('');
+    inputRef.current.focus();
+  };
 
-    useEffect(() => {
-        dispatch(getTasks(projectId));
-    }, [dispatch, projectId]);
-
-    function toggleForm(): void {
-        setToggleTaskForm(true)
+  function handleDayChange(date, modifiers) {
+    if (modifiers.disabled) {
+      return;
     }
+    setSelectedDay(date)
+  }
 
-    function closeForm(): void {
-        setToggleTaskForm(false);
-    }
+  const dropdownItems = [{
+    name: 'Show completed tasks',
+  }]
 
-    function openDropdown(): void {
-        setDropdown(true);
-    }
+  const incompletedTasks = tasks.filter(task => task.completed !== true);
 
-    function closeDropdown(): void {
-        setDropdown(false);
-    }
-
-    function handleChange(e: { target: { value: React.SetStateAction<string>; }; }): void {
-        setTaskValue(e.target.value);
-    }
-
-    function createTaskRequest(e: { preventDefault: () => void; }): void {
-        e.preventDefault();
-        const newTask = {
-            task: taskValue,
-            date: new Date(),
-            schedule: selectedDay,
-            projectName,
-            completed: false,
-            archived: isArchived,
-            userId
-        }
-        dispatch(createTask(projectId, newTask));
-        setTaskValue('');
-        inputRef.current.focus();
-    };
-
-    function handleDayChange(date, modifiers) {
-        if (modifiers.disabled) {
-            return;
-        }
-        setSelectedDay(date)
-    }
-
-    const dropdownItems = [{
-        name: 'Show completed tasks',
-    }]
-
-    const incompletedTasks = tasks.filter(task => task.completed !== true);
-
-    return (
-        <>
-            <div className={style.tasks}>
-                <div className={style.tasksTitleContainer} >
-                    <div>
-                        <h1>{projectName}</h1>
-                        {isArchived && <span>Archived</span>}
-                    </div>
-                    <span onClick={openDropdown} className={style.tasksTitleContainerDropdownAction} >
-                        <IoIosMore />
-                        {dropdown && <Dropdown
-                            closeDropdown={closeDropdown}
-                            dropdownItems={dropdownItems}
-                            left='0'
-                            top='0'
-                        />}
-                    </span>
-                </div>
-                {tasks.length > 0 ? tasks && incompletedTasks.map(task => {
-                    return <ul key={task.id}>
-                        <Task
-                            projectId={projectId}
-                            task={task}
-                            isArchived={isArchived}
-                            selectedDay={selectedDay}
-                            handleDayChange={handleDayChange}
-                            inputRef={inputRef}
-                        />
-                    </ul>
-                }) : null}
-                {toggleTaskForm ? <TaskForm
-                    buttonDo='Add Task'
-                    buttonClose='Cancel'
-                    onClickClose={closeForm}
-                    onChange={handleChange}
-                    inputValue={taskValue}
-                    request={createTaskRequest}
-                    selectedDay={selectedDay}
-                    handleDayChange={handleDayChange}
-                    inputRef={inputRef}
-                />
-                    : !isArchived && <TaskCreator onClick={toggleForm}
-                    />}
-                {errors.error && <Error textError={errors.error} />}
-                <TasksHistory projectId={projectId} tasks={tasks} />
-                <Toast showToast={showToast} text={toastText} backgroundColor='#383838' />
-            </div>
-            {isLoading && <div className='overlay'>
-                <img src={spinner} alt='spinner' />
-            </div>}
-        </>
-    );
+  return (
+    <>
+      <div className={style.tasks}>
+        <div className={style.tasksTitleContainer} >
+          <div>
+            <h1>{projectName}</h1>
+            {isArchived && <span>Archived</span>}
+          </div>
+          <span onClick={openDropdown} className={style.tasksTitleContainerDropdownAction} >
+            <IoIosMore />
+            {dropdown && <Dropdown
+              closeDropdown={closeDropdown}
+              dropdownItems={dropdownItems}
+              left='0'
+              top='0'
+            />}
+          </span>
+        </div>
+        {tasks.length > 0 ? tasks && incompletedTasks.map(task => {
+          return <ul key={task.id}>
+            <Task
+              projectId={projectId}
+              task={task}
+              isArchived={isArchived}
+              selectedDay={selectedDay}
+              handleDayChange={handleDayChange}
+              inputRef={inputRef}
+            />
+          </ul>
+        }) : null}
+        {toggleTaskForm ? <TaskForm
+          buttonDo='Add Task'
+          buttonClose='Cancel'
+          onClickClose={closeForm}
+          onChange={handleChange}
+          inputValue={taskValue}
+          request={createTaskRequest}
+          selectedDay={selectedDay}
+          handleDayChange={handleDayChange}
+          inputRef={inputRef}
+        />
+          : !isArchived && <TaskCreator onClick={toggleForm}
+          />}
+        {errors.error && <Error textError={errors.error} />}
+        <TasksHistory projectId={projectId} tasks={tasks} />
+        <Toast showToast={showToast} text={toastText} backgroundColor='#383838' />
+      </div>
+      {isLoading && <div className='overlay'>
+        <img src={spinner} alt='spinner' />
+      </div>}
+    </>
+  );
 }
