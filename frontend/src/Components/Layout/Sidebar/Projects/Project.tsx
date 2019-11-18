40efbf6f00befa29fb2e244639b08128e44ceb0b
Dragos Ifrim <dragos.ifrim@softvision.ro>
2019-11-18 10:04:00 +0200

    Fixed bug when editing a project: Input lost value after the first character inserted in the input

--- frontend/src/Components/Layout/Sidebar/Projects/Project.tsx
+++ frontend/src/Components/Layout/Sidebar/Projects/Project.tsx
@@ -21,148 +21,148 @@ import { Modal } from '../../../modal/Modal';
 import { ModalDropdown } from '../../../modal/ModalDropdown';
 
 export const Project = ({ project, userId, history, areArchivedProjects, isArchived }): JSX.Element => {
-    const [dropdown, setDropdown] = useState(false);
-    const [modal, setModal] = useState(false);
-    const [modalDropdown, setModalDropdown] = useState(false);
-    const [projectValueEdit, setProjectValueEdit] = useState(project.name);
+  const [dropdown, setDropdown] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [modalDropdown, setModalDropdown] = useState(false);
+  const [projectValueEdit, setProjectValueEdit] = useState(project.name);
 
-    //redux
-    const dispatch = useDispatch();
+  //redux
+  const dispatch = useDispatch();
 
-    //DROPDOWN TOGGLE
-    function openDropdown(): void {
-        setDropdown(true);
+  //DROPDOWN TOGGLE
+  function openDropdown(): void {
+    setDropdown(true);
+  }
+
+  function closeDropdown(): void {
+    setDropdown(false)
+  }
+
+  //MODAL TOGGLE
+  function openModal(): void {
+    closeDropdown();
+    setModal(true);
+  }
+
+  function closeModal(): void {
+    setModal(false)
+  }
+
+  //MODAL FROM DROPDOWN TOGGLE
+  function openModalDropdown(): void {
+    setModalDropdown(true);
+    closeDropdown();
+  }
+
+  function closeModalDropdown(): void {
+    setModalDropdown(false);
+  }
+
+  function onChange(e: { target: { value: string; }; }): void {
+    setProjectValueEdit(e.target.value);
+  }
+
+  function removeSelectedProject(): void {
+    dispatch(removeProject(userId, project.id));
+    history.push('/')
+  }
+
+  function editSelectedProject(e: { preventDefault: () => void; }): void {
+    e.preventDefault();
+    const newProject = {
+      name: projectValueEdit,
+      archived: false,
     }
+    dispatch(editProject(userId, project.id, newProject));
+    setModal(false);
+  }
 
-    function closeDropdown(): void {
-        setDropdown(false)
-    }
+  function setProjectArchived(): void {
+    project = {
+      ...project,
+      archived: true,
+    };
 
-    //MODAL TOGGLE
-    function openModal(): void {
-        setModal(true);
-    }
+    dispatch(updateAllTasks(project.id, project.name, true))
+    dispatch(editProject(userId, project.id, project));
+    setDropdown(false);
+  }
 
-    function closeModal(): void {
-        setModal(false)
-    }
+  function setProjectUnarchived(): void {
 
-    //MODAL FROM DROPDOWN TOGGLE
-    function openModalDropdown(): void {
-        setModalDropdown(true);
-        closeDropdown();
-    }
+    project = {
+      ...project,
+      archived: false,
+    };
 
-    function closeModalDropdown(): void {
-        setModalDropdown(false);
-    }
+    dispatch(updateAllTasks(project.id, project.name, false))
+    dispatch(editProject(userId, project.id, project));
+    setDropdown(false);
+  }
 
-    function onChange(e: { target: { value: string; }; }): void {
-        setProjectValueEdit(e.target.value);
-    }
+  const editIcon: JSX.Element = <IoMdCreate />;
+  const archiveIcon: JSX.Element = <IoIosArchive />;
+  const removeIcon: JSX.Element = <IoIosTrash />;
 
-    function removeSelectedProject(): void {
-        dispatch(removeProject(userId, project.id));
-        history.push('/')
-    }
+  const dropdownItemsUnarchived = [{
+    name: 'Edit project',
+    action: openModal,
+    icon: editIcon,
+  },
+  {
+    name: 'Archive project',
+    action: () => setProjectArchived(),
+    icon: archiveIcon,
+  },
+  {
+    name: 'Remove project',
+    action: openModalDropdown,
+    icon: removeIcon,
+  }
+  ];
 
-    function editSelectedProject(e: { preventDefault: () => void; }): void {
-        e.preventDefault();
-        const newProject = {
-            name: projectValueEdit,
-            archived: false,
-        }
-        dispatch(editProject(userId, project.id, newProject));
-        setModal(false);
-    }
+  const dropdownItemsArchived = [{
+    name: 'Unarchive project',
+    action: () => setProjectUnarchived(),
+    icon: archiveIcon,
+  },
+  {
+    name: 'Remove project',
+    action: openModalDropdown,
+    icon: removeIcon,
+  }];
 
-    function setProjectArchived(): void {
-        project = {
-            ...project,
-            archived: true,
-        };
+  const question: string = `Are you sure you want to remove ${project.name}?`;
 
-        dispatch(updateAllTasks(project.id, project.name, true))
-        dispatch(editProject(userId, project.id, project));
-        setDropdown(false);
-    }
+  return (
+    <>
+      <li className={style.projectItem} >
+        <Link to={{ pathname: `/project/${project.id}`, search: project.name, state: isArchived }}>
+          <span className='dot'></span>
+          <span>{project.name}</span>
+        </Link>
+        <span className={style.projectItemSettings} onClick={openDropdown}><IoIosMore /></span>
+        {dropdown && <Dropdown
+          closeDropdown={closeDropdown}
+          dropdownItems={areArchivedProjects ? dropdownItemsArchived : dropdownItemsUnarchived}
+          left='97.5'
+          top='70'
+        />}
+      </li>
 
-    function setProjectUnarchived(): void {
-
-        project = {
-            ...project,
-            archived: false,
-        };
-
-        dispatch(updateAllTasks(project.id, project.name, false))
-        dispatch(editProject(userId, project.id, project));
-        setDropdown(false);
-    }
-
-    const editIcon: JSX.Element = <IoMdCreate />;
-    const archiveIcon: JSX.Element = <IoIosArchive />;
-    const removeIcon: JSX.Element = <IoIosTrash />;
-
-    const dropdownItemsUnarchived = [{
-        name: 'Edit project',
-        action: openModal,
-        icon: editIcon,
-    },
-    {
-        name: 'Archive project',
-        action: () => setProjectArchived(),
-        icon: archiveIcon,
-    },
-    {
-        name: 'Remove project',
-        action: openModalDropdown,
-        icon: removeIcon,
-    }
-    ]
-
-
-    const dropdownItemsArchived = [{
-        name: 'Unarchive project',
-        action: () => setProjectUnarchived(),
-        icon: archiveIcon,
-    },
-    {
-        name: 'Remove project',
-        action: openModalDropdown,
-        icon: removeIcon,
-    }];
-
-    const question: string = `Are you sure you want to remove ${project.name}?`;
-
-    return (
-        <>
-            <li className={style.projectItem} >
-                <Link to={{ pathname: `/project/${project.id}`, search: project.name, state: isArchived }}>
-                    <span className='dot'></span>
-                    <span>{project.name}</span>
-                </Link>
-                <span className={style.projectItemSettings} onClick={openDropdown}><IoIosMore /></span>
-                {dropdown && <Dropdown
-                    closeDropdown={closeDropdown}
-                    dropdownItems={areArchivedProjects ? dropdownItemsArchived : dropdownItemsUnarchived}
-                    left='97.5'
-                    top='70'
-                />}
-            </li>
-
-            {modalDropdown && <ModalDropdown
-                question={question}
-                closeModal={closeModalDropdown}
-                request={removeSelectedProject}
-            />}
-            {modal && <Modal
-                onChange={onChange}
-                closeModal={closeModal}
-                inputValue={projectValueEdit}
-                labelName='Project name'
-                title='Edit project'
-                request={editSelectedProject}
-                buttonDo='Edit' />}
-        </>
-    )
+      {modalDropdown && <ModalDropdown
+        question={question}
+        closeModal={closeModalDropdown}
+        request={removeSelectedProject}
+      />}
+      {modal && <Modal
+        onChange={onChange}
+        closeModal={closeModal}
+        inputValue={projectValueEdit}
+        labelName='Project name'
+        title='Edit project'
+        request={editSelectedProject}
+        buttonDo='Edit' />}
+    </>
+  )
 };
